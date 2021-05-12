665cdad5d5e9f2a4048394e2341d673f94b53c84
James Sanchez Seaton Reid <mr_jimmybob@yahoo.com>
2021-05-12 15:45:43 +0100

    test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using EncryptorLibrary;
-
+ 
 namespace findcnf 
 {
     class Program
